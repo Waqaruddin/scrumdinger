7d138a0dd0710058da6fe1d56a1f4a0031d0e581
Syed Uddin <syeduddin297@gmail.com>
2022-09-28 21:54:46 -0400

    Initial Commit

---
+++ Srumdinger/SrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SrumdingerApp.swift
+//  Srumdinger
+//
+//  Created by Syed Waqar Uddin on 9/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
